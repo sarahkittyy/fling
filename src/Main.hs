94aa418fbe87e93e7fcc7ac607f8e2379f8eefe8
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-01 15:28:37 -0400

    repl is called from main with -i arg

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,35 @@
 module Main where
+    
+import Fling.Interpreter
+import Fling.Repl
+import Opts.Parse
+import Opts.Grammar
+
+import Control.Monad (when)
+import System.Environment
+
+flagArgs :: [String]
+flagArgs = [ "i", "interactive"
+           , "h", "help" ]
+           
+valArgs :: [String]
+valArgs = [  ]
+
+program :: Program
+program = Program { flagOnly=flagArgs
+                  , valueOnly=valArgs }
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    opts <- fromOpts program <$> getArgs
+    case opts of
+        Left err -> putStrLn err >> return () 
+        Right opts -> main_v opts
+    where
+        main_v :: [Option] -> IO ()
+        main_v opts = do
+            when (hasAnyOpt ["i", "interactive"] opts) (putStrLn intro >> (repl newEnv))
+                
+            return ()       
+
+
