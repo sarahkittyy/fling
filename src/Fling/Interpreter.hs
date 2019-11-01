6139770d8b1cc47543b8dabceb3745f4ef40c8a1
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-01 13:36:26 -0400

    implemented basic interpreter data

--- src/Fling/Interpreter.hs
+++ src/Fling/Interpreter.hs
@@ -1,2 +1,17 @@
 -- | Defines the interpreter engine for fling.
 module Fling.Interpreter where
+    
+import Fling.Grammar
+
+-- | The main environment, where all persistent data is stored
+data Env = Env {
+
+} deriving (Show)
+
+newEnv :: Env
+newEnv = Env {  }
+
+-- | Reduces an expression to a literal
+evalExpr :: Expression -> Literal
+evalExpr (Literal l) = l 
+evalExpr (Wrapped expr) = evalExpr expr
